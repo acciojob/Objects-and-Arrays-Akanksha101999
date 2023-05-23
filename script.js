50bd6458e80d73fdeffd69db93d1e9b3a81aecd6
Akanksha101999 <97754410+Akanksha101999@users.noreply.github.com>
2023-05-23 13:07:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ const person = {
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Write your code here, make sure the name of variables is exactly same as mentioned in the problemstatement
+const team = players;
+const team1 = Array.from(players);
+
+const cap1 = {};
+for(let key in person){
+	cap1[key] = person[key];
+}
 
 window.players = players;
 window.person = person;
